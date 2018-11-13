e55a716c7834940199131329e379b656910316a2
Christoph Parnitzke <christoph.parnitzke@tu-dortmund.de>
2018-11-13 17:15:59 +0100

    use router to activate sidenav elements

--- librecarsharing/src/app/app.component.ts
+++ librecarsharing/src/app/app.component.ts
@@ -6,23 +6,8 @@ import { getHostElement } from '@angular/core/src/render3';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent implements OnInit {
+export class AppComponent {
   title = 'librecarsharing';
-  isDashboardActive: boolean;
-  isCarsActive: boolean;
-  isUsersActive: boolean;
-  isAdminActive: boolean;
-  
-  ngOnInit() {
-    this.isDashboardActive = true;
-    this.isCarsActive = false;
-    this.isUsersActive = false;
-    this.isAdminActive = false;
-  }
-  
-  OnClick() {
-    console.log("Onclick()")
-  }
 
   logout() {
     window.location.href = "http://localhost:8080/auth/realms/testing/protocol/openid-connect/logout?redirect_uri=http%3A%2F%2Flocalhost%3A4200";
